c3520b83526e920a6fc37c831eed871a8ec956eb
Dmitriy Pismenniy <dima30655@gmail.com>
2020-10-04 21:29:34 +0300

    Updates

---
+++ precache-manifest.83232e893a695550d02396427072cb88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060508fcd1d6b7aa4f30392110fe6d30",
+    "url": "/text_pio/index.html"
+  },
+  {
+    "revision": "9e5386627e931ebe13e3",
+    "url": "/text_pio/static/css/main.755ac6d4.chunk.css"
+  },
+  {
+    "revision": "462f36f4979800e9f725",
+    "url": "/text_pio/static/js/2.58ffd5ad.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/text_pio/static/js/2.58ffd5ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5386627e931ebe13e3",
+    "url": "/text_pio/static/js/main.3fa1dae4.chunk.js"
+  },
+  {
+    "revision": "25be2a0b64b7116286d1",
+    "url": "/text_pio/static/js/runtime-main.c9942dc7.js"
+  }
+]);
